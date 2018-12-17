aba9ca4ecba3e74a076f1a3e018b8cb4a76e2741
olivergoodman <olivergoodman2017@u.northwestern.edu>
2018-12-16 22:07:08 -0800

    basic stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+function showLinks() {
+  var x = document.getElementById("resume-link");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
