3549ce083f56c3d838c83577ae373c7fba9c8c27
olivergoodman <olivergoodman2017@u.northwestern.edu>
2018-12-20 11:26:43 -0800

    progress

--- script.js
+++
@@ -1,8 +0,0 @@
-function showLinks() {
-  var x = document.getElementById("resume-link");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
