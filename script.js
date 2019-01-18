bd6cd85d5d78d068049847ffda649365955e34c7
olivergoodman <olivergoodman2017@u.northwestern.edu>
2019-01-17 21:38:49 -0800

    yep

---
+++ script.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', ['ngAnimate']);
+app.controller('myCtrl', function($scope) {
+    $scope.myName= "Oliver Goodman";
+
+    $scope.myLinks = [
+    	{text: "LinkedIn", url: "https://www.linkedin.com/in/oliverbgoodman/"},
+    	{text: "Github", url: "https://github.com/olivergoodman/"},
+    	{text: "Email", url: "mailto:oliverbgoodman@gmail.com"},
+    	{text: "CV", url: "Oliver_Goodman_Resume.pdf"}
+    ];
+
+    $scope.letterboxd = {text: "movies", url:"https://letterboxd.com/ollygreatman/"}
+
+    $scope.showBio = false;
+
+    $scope.clickHiddenLink = function(link) {
+    	if ($scope.showBio == true) {
+    		return link.url;
+    	} else {
+    		return ""
+    	}
+    }
+
+})
